1756442cbb3618e3cca1b5293579b587fbd26f6f
MattKazen <Matthew.Kazen@gmail.com>
2018-01-29 18:00:11 -0500

    Firebase Register Functionality Working

--- app/src/main/java/CKD/Android/registerNewUser.java
+++ app/src/main/java/CKD/Android/registerNewUser.java
@@ -1,61 +1,72 @@
 package CKD.Android;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class registerNewUser extends AppCompatActivity {
 
     public EditText name;
     private String mDisplayName;
     private Button Register;
+    private FirebaseAuth mAuth;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_new_user);
 
-        final EditText name = (EditText) findViewById(R.id.nameTF);
-        Button Register = (Button) findViewById(R.id.registerNow);
+        // UI Components
+        final EditText Email =  findViewById(R.id.TF_EmailAddress);
+        final EditText Password = findViewById(R.id.TF_password);
+        Button Register = findViewById(R.id.btn_register);
 
         // OnClick Listener that redirects to Register Page
         Register.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
 
-                // Convert Text to String and store it it in var mDisplayName
-                mDisplayName = name.getText().toString();
-                // Get Instance of Database
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                // Child Name
-                DatabaseReference myRef = database.getReference("NAME");
-                // Set Value to whatever name user Enters
-                myRef.setValue(mDisplayName);
+                String newEmail = Email.getText().toString();
+                String newPassword= Password.getText().toString();
 
+                mAuth = FirebaseAuth.getInstance();
+
+                Task<AuthResult> password =
+                                 mAuth.createUserWithEmailAndPassword(newEmail, newPassword)
+                                .addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        // User Successfully Registered to database
+                        if (task.isSuccessful())
+                        {
+                            AuthResult result = task.getResult();
+                            Intent launchActivity1= new Intent(
+                                    CKD.Android.registerNewUser.this,MainActivity.class);
+                            startActivity(launchActivity1);
+
+                        }
+                        // User unable to register_class user to database
+                        else
+                        {
+                            @SuppressWarnings("ThrowableNotThrown")
+                            Exception exception = task.getException();
+                        }
+
+                    }
+                });
             }
         });
-
-        // Read from the database
-        /*myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });*/
     }
 }
