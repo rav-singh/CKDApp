d1686451c2e529108dd3cc919736910e015a654b
MattKazen <Matthew.Kazen@gmail.com>
2018-01-30 15:17:28 -0500

    References AppData Class for Firebase

--- app/src/main/java/CKD/Android/Demographics.java
+++ app/src/main/java/CKD/Android/Demographics.java
@@ -7,23 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
 
 public class Demographics extends AppCompatActivity
 {
-    private FirebaseUser mAuth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demographics);
 
-        mAuth = FirebaseAuth.getInstance().getCurrentUser();
 
         // UI Components
         final EditText Name = findViewById(R.id.Demo_TF_Name);
@@ -36,8 +31,8 @@ public class Demographics extends AppCompatActivity
 
         //  Fills in Name and Email Data for profile
         // May be excessive and might simplify by removing name from Register Page
-        Email.setText(mAuth.getEmail());
-        UID.setText(mAuth.getUid());
+        Email.setText(AppData.getInstance().firebaseUser.getEmail());
+        UID.setText(AppData.getInstance().firebaseUser.getUid());
 
         // OnClick Listener that redirects to homePage
         Register.setOnClickListener(new View.OnClickListener()
@@ -69,7 +64,7 @@ public class Demographics extends AppCompatActivity
     private void addUserToDatabase(String userName, String userEmail, String phone, String age, String activity, String UID)
     {
 
-        final FirebaseDatabase db = FirebaseDatabase.getInstance();
+       FirebaseDatabase db = AppData.getInstance().db;
 
         DatabaseReference User_node = db.getReference("Users");
 
