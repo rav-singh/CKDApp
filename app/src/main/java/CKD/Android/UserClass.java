e1ebc3aa680fc40a1281e0d5c8ffba7fe8d163f5
MattKazen <Matthew.Kazen@gmail.com>
2018-01-30 13:42:41 -0500

    Added Class Variables to User Class

--- app/src/main/java/CKD/Android/UserClass.java
+++ app/src/main/java/CKD/Android/UserClass.java
@@ -5,45 +5,40 @@ package CKD.Android;
 public class UserClass
 {
 
-    private String Name;
-    private String Email;
-    private String Phone;
+    private String name;
+    private String email;
+    private String phone;
+    private String age;
+    private String activityLevel;
     private String UID;
 
-    public UserClass()
+    public UserClass(String name, String email, String phone, String age, String activityLevel, String UID )
     {
-
-    }
-    public void setName(String Name)
-    {
-        this.Name = Name;
-    }
-
-    public void setEmail(String Email){
-        this.Email = Email;
-    }
-
-    public void setPhone(String Phone){
-        this.Phone = Phone;
-    }
-
-    public void setUID(String UID){
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.age = age;
+        this.activityLevel =  activityLevel;
         this.UID = UID;
     }
 
+
     public String getName(){
-        return Name;
+        return name;
     }
 
     public String getEmail(){
-        return Email;
+        return email;
     }
 
     public String getPhone(){
-        return Phone;
+        return phone;
     }
 
-    public String UID(){
-        return UID;
-    }
+    public String getAge(){return age;}
+
+    public String getActivityLevel(){return activityLevel;}
+
+    public String getUID(){return UID;}
+
 }
