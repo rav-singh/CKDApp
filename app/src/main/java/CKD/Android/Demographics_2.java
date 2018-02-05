8238f5c5f4ff022aee26e82d9c1129a68a536cbc
MattKazen <Matthew.Kazen@gmail.com>
2018-02-04 20:04:31 -0500

    User successfully Registers and uploads data to database

--- app/src/main/java/CKD/Android/Demographics_2.java
+++ app/src/main/java/CKD/Android/Demographics_2.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -26,6 +27,9 @@ import java.util.List;
 
 public class Demographics_2 extends AppCompatActivity
 {
+
+   FirebaseAuth Authenticator = FirebaseAuth.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -42,15 +46,12 @@ public class Demographics_2 extends AppCompatActivity
         final CheckBox race4 = findViewById(R.id.Demo_CB_Race_4);
         final CheckBox race5 = findViewById(R.id.Demo_CB_Race_5);
         final CheckBox race6 = findViewById(R.id.Demo_CB_Race_6);
-        final List<String> raceList = new ArrayList<String>();
+        final List<String> raceList = new ArrayList<>();
 
         next.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
-                // checked makes sure that each race node gets a unique value associated with it,
-                // so that we can insert multiple races.
-
                 // Grabs all user input into strings
                 String _Age = userAge.getSelectedItem().toString();
                 String _Marital = userMarital.getSelectedItem().toString();
@@ -64,14 +65,12 @@ public class Demographics_2 extends AppCompatActivity
 
                 //TODO Clear password in User Class once its used
                 // Create User in Firebase Authentication
-                // Email and Password were set to this class in registerNewUserClass
-                createUserAuth(AppData.cur_user.getEmail(), AppData.cur_user.getPassword());
-
-                //Update AppData mAuth Reference
-                AppData.getInstance().setmAuth(FirebaseAuth.getInstance());
+                createUserAuth(AppData.cur_user.getEmail(),
+                        AppData.cur_user.getPassword());
 
                 // Adds User to RealTime Database under Users Node
-                addCurrentUserToDatabase();
+
+
                 // Directs User to HomePage
                 switchPages();
             }
@@ -131,105 +130,81 @@ public class Demographics_2 extends AppCompatActivity
         });
     }
 
-    private void addCurrentUserToDatabase()
+    protected void addCurrentUserToDatabase(UserClass cur_user)
     {
         //Grabs references to the Root node and Users Node
         FirebaseDatabase db = FirebaseDatabase.getInstance();
-        DatabaseReference User_node = db.getReference("Users");
-
-        // Saves the newly generated UID from Firebase into UserClass
-        AppData.cur_user.setUID(AppData.getInstance().getmAuth().getCurrentUser().getUid());
-
-        //Creates a copy of the current user in AppData Class
-        UserClass tempUser = AppData.cur_user;
-
-        String UID = tempUser.getUID();
 
         //Creates a UID Node under Users
-        DatabaseReference UID_node = User_node.child(UID);
-        //Creates a node under UID
-        DatabaseReference Add_node = User_node.child(UID).child("Additional");
-        DatabaseReference Race_node = User_node.child(UID).child("Race");
 
-        UID_node.child("Name").setValue(tempUser.getName());
-        UID_node.child("Email").setValue(tempUser.getEmail());
-        UID_node.child("Phone Number").setValue(tempUser.getPhone());
-        Add_node.child("Activity Level").setValue(tempUser.getActivityLevel());
-        Add_node.child("Marital Status").setValue(tempUser.getMarital());
-        Add_node.child("Gender").setValue(tempUser.getGender());
+        DatabaseReference User_node = db.getReference("Users");
 
-        for(int i = 0; i< tempUser.getRace().size(); i++)
-        {
-            Race_node.child(String.valueOf(i)).setValue(tempUser.getRace().get(i));
-        }
-    }
+        String UID = cur_user.getUID();
 
-
-    private void addCurrentUserToDatabasePrevious()
-    {
-
-
-/*
         DatabaseReference UID_node = User_node.child(UID);
         DatabaseReference Add_node = User_node.child(UID).child("Additional");
         DatabaseReference Race_node = User_node.child(UID).child("Race");
 
+        UID_node.child("Name").setValue(AppData.cur_user.getName());
+        UID_node.child("Email").setValue(AppData.cur_user.getEmail());
+        UID_node.child("Phone Number").setValue(AppData.cur_user.getPhone());
+        Add_node.child("Activity Level").setValue(AppData.cur_user.getActivityLevel());
+        Add_node.child("Marital Status").setValue(AppData.cur_user.getMarital());
+        Add_node.child("Gender").setValue(AppData.cur_user.getGender());
 
-        UID_node.child("Name").setValue("Just Checking");
-        UID_node.child("Email").setValue("If working");
-        UID_node.child("Phone Number").setValue("Properly");
-        Add_node.child("Activity Level").setValue("Don't mind");
-        Add_node.child("Marital Status").setValue("me");
-        Add_node.child("Gender").setValue("Testing");
-
-        for(int i = 0; i< currentUser.getRace().size(); i++)
+        for(int i = 0; i< AppData.cur_user.getRace().size(); i++)
         {
-            Race_node.child(String.valueOf(i)).setValue(raceList.get(i));
+            Race_node.child(String.valueOf(i)).setValue(AppData.cur_user.getRace().get(i));
         }
-*/
     }
-
 
     private void createUserAuth(String newEmail, String newPassword)
     {
-        //TODO Change back to AppData Reference
         Task<AuthResult> Auth =
-                            FirebaseAuth.getInstance().createUserWithEmailAndPassword(newEmail, newPassword)
-                            .addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                    Authenticator.createUserWithEmailAndPassword(newEmail, newPassword)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>()
                              {
                                     @Override
                                 public void onComplete(@NonNull Task<AuthResult> task)
                                 {
-                                    // User Successfully Registered to database
-                                    if (task.isSuccessful())
-                                    {
-                                        AuthResult result = task.getResult();
-                                    }
-                                    // User unable to register_class user to database
-                                    else
-                                    {
-                                        // print out error when we can't register user
-                                        String error_Message = task.getException().toString();
-                                        if(error_Message.contains("password"))
-                                        {
-                                            Toast.makeText(Demographics_2.this,
-                                                    "Sorry but your password must be atleast 6 characters long",
-                                                    Toast.LENGTH_LONG).show();
-                                        }
-                                        else if(error_Message.contains("email"))
-                                        {
-                                            Toast.makeText(Demographics_2.this,
-                                                    "Invalid Email!", Toast.LENGTH_LONG).show();
-                                        }
-                                        else
-                                        {
-                                            Toast.makeText(Demographics_2.this,
-                                                            error_Message,
-                                                            Toast.LENGTH_LONG).show();
-                                        }
-                                    }
+                            // User Successfully Registered to database
+                            if (task.isSuccessful())
+                            {
+                                AuthResult result = task.getResult();
+
+                                AppData.setmAuth(Authenticator);
+                                AppData.setDb(FirebaseDatabase.getInstance());
+                                AppData.setFirebaseUser(Authenticator.getCurrentUser());
+                                AppData.cur_user.setUID(AppData.firebaseUser.getUid());
+                                AppData.cur_user.clearPassword();
+                                addCurrentUserToDatabase(AppData.cur_user);
+                            }
+                            // User unable to register_class user to database
+                            else
+                            {
+                                // print out error when we can't register user
+                                String error_Message = task.getException().toString();
+                                if(error_Message.contains("password"))
+                                {
+                                    Toast.makeText(Demographics_2.this,
+                                            "Sorry but your password must be atleast 6 characters long",
+                                            Toast.LENGTH_LONG).show();
                                 }
-                            });
+                                else if(error_Message.contains("email"))
+                                {
+                                    Toast.makeText(Demographics_2.this,
+                                            "Invalid Email!", Toast.LENGTH_LONG).show();
+                                }
+                                else
+                                {
+                                    Toast.makeText(Demographics_2.this,
+                                                    error_Message,
+                                                    Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        }
+                    });
+
     }
 
     private void switchPages()
@@ -246,23 +221,5 @@ public class Demographics_2 extends AppCompatActivity
         AppData.cur_user.setRace(races);
     }
 
-    private void addRaceToDatabase(String userRace, int i, String UID)
-    {
-        FirebaseDatabase db = AppData.db;
-        DatabaseReference User_node = db.getReference("Users");
-        DatabaseReference Race_node = User_node.child(UID).child("Race");
-        Race_node.child(String.valueOf(i)).setValue(userRace);
-    }
-
-    private void addUserToDatabase(String userAge, String userMarital, String userGender, String UID)
-    {
-        FirebaseDatabase db = AppData.db;
-        DatabaseReference User_node = db.getReference("Users");
-        DatabaseReference Add_node = User_node.child(UID).child("Additional");
-
-        User_node.child(UID).child("Age").setValue(userAge);
-        Add_node.child("Marital Status").setValue(userMarital);
-        Add_node.child("Gender").setValue(userGender);
-    }
 }
 
