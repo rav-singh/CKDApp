cd4727f23e9ee06ecb190ed2228a8be7d8142460
kerristanne <madeleine.cassidy@knights.ucf.edu>
2018-02-10 12:16:08 -0500

    Created Firebase Hosted Website and initial pages

---
+++ website/functions/index.js
@@ -0,0 +1,12 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
