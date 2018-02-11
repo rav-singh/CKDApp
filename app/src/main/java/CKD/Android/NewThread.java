08fe0ca96acd407ae3cf139f1a74cd1c6bcb8b21
MattKazen <Matthew.Kazen@gmail.com>
2018-02-11 15:45:43 -0500

    Writes Thread Information into Thread Class and Database

--- app/src/main/java/CKD/Android/NewThread.java
+++ app/src/main/java/CKD/Android/NewThread.java
@@ -1,16 +1,22 @@
 package CKD.Android;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class NewThread extends AppCompatActivity
@@ -21,22 +27,58 @@ public class NewThread extends AppCompatActivity
         super.onCreate(savedInstanceState) ;
         setContentView(R.layout.activity_new_thread);
 
-        EditText threadTitle = findViewById(R.id.NewThread_TE_Title);
-        EditText threadBody=  findViewById(R.id.NewThread_TE_Body);
+        final EditText threadTitle = findViewById(R.id.NewThread_TE_Title);
+        final EditText threadBody=  findViewById(R.id.NewThread_TE_Body);
         Button submit_btn = findViewById(R.id.NewThread_Btn_Submit);
-
 
         submit_btn.setOnClickListener(new View.OnClickListener()
         {
 
             public void onClick(View v)
             {
-                //TODO Place function that adds the new Thread to the database AND updates the ThreadlistPage
+                String authorUID = AppData.cur_user.getUID();
+                String authorName = AppData.cur_user.getName();
+                String title = threadTitle.getText().toString();
+                String body = threadBody.getText().toString();
+                String category = AppData.cur_Category;
+                String date = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss",
+                        Locale.getDefault()).format(new Date());
+
+                ThreadClass newThread = new ThreadClass(authorName,authorUID,title,body,date,category);
+
+                FirebaseDatabase db;
+
+                if(AppData.db == null)
+                {
+                    db = FirebaseDatabase.getInstance();
+                    AppData.db = db;
+                }
+                else
+                    db = AppData.db;
+
+                //References the Node under data and the current category the user is in
+                DatabaseReference Category_node = db.getReference("Data").child("Social").child(category);
+                DatabaseReference Thread_node = Category_node.child("THREAD2");
+
+                Thread_node.setValue(newThread);
+
+
+
+
+
+
                 Intent launchActivity1 =
                         new Intent(CKD.Android.NewThread.this,ThreadsList.class);
                 startActivity(launchActivity1);
             }
+
+
         });
 
     }
+
+
+
+
+
 }
