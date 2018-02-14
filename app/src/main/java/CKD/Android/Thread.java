9ac8f8d8d255f47967abdda36a3c60de1f65f402
MattKazen <Matthew.Kazen@gmail.com>
2018-02-14 15:19:46 -0500

    Threads list and Threads Comments dynamically added to pages

--- app/src/main/java/CKD/Android/Thread.java
+++ app/src/main/java/CKD/Android/Thread.java
@@ -24,6 +24,8 @@ import java.util.List;
 public class Thread extends AppCompatActivity
 {
     final List<String> keyList= new ArrayList<>();
+    final List<TextView> cmntList= new ArrayList<>();
+    final List<TextView> authList= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +54,11 @@ public class Thread extends AppCompatActivity
     private void getComments()
     {
         FirebaseDatabase db = FirebaseDatabase.getInstance();
-        DatabaseReference Comments_node = db.getReference("Data").child("Social").child(AppData.cur_Category).child(AppData.cur_Thread_Key).child("Comments");
+        DatabaseReference Comments_node = db.getReference("Data")
+                                            .child("Social")
+                                            .child(AppData.cur_Category)
+                                            .child(AppData.cur_Thread_Key)
+                                            .child("Comments");
 
         Comments_node.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -61,11 +67,32 @@ public class Thread extends AppCompatActivity
                 for(DataSnapshot d : dataSnapshot.getChildren())
                 {
                     keyList.add(d.getKey());
-
                 }
 
                 TextView Comment1 = findViewById(R.id.Thread_TV_Comment1);
                 TextView Comment1Auth = findViewById(R.id.Thread_TV_Cmnt1_author);
+                cmntList.add(Comment1);
+                authList.add(Comment1Auth);
+
+                TextView Comment2 = findViewById(R.id.Thread_TV_Comment2);
+                TextView Comment2Auth = findViewById(R.id.Thread_TV_Cmnt2_author);
+                cmntList.add(Comment2);
+                authList.add(Comment2Auth);
+
+                TextView Comment3 = findViewById(R.id.Thread_TV_Comment3);
+                TextView Comment3Auth = findViewById(R.id.Thread_TV_Cmnt3_author);
+                cmntList.add(Comment3);
+                authList.add(Comment3Auth);
+
+                TextView Comment4 = findViewById(R.id.Thread_TV_Comment4);
+                TextView Comment4Auth = findViewById(R.id.Thread_TV_Cmnt4_author);
+                cmntList.add(Comment4);
+                authList.add(Comment4Auth);
+
+                TextView Comment5 = findViewById(R.id.Thread_TV_Comment5);
+                TextView Comment5Auth = findViewById(R.id.Thread_TV_Cmnt5_author);
+                cmntList.add(Comment5);
+                authList.add(Comment5Auth);
 
                 //If there are no comments on this thread
                 if (keyList.size() == 0)
@@ -73,9 +100,10 @@ public class Thread extends AppCompatActivity
                     Comment1.setText("There are no comments on this thread");
                     return;
                 }
-                //TODO Implement a way to dynamically fill all/some of the comment boxes
-                fillInComments(Comment1, Comment1Auth,0,dataSnapshot);
-
+                for(int i = keyList.size()-1; i >=0 ; i--)
+                {
+                    fillInComments(cmntList.get(i), authList.get(i), i, dataSnapshot);
+                }
             }
 
             @Override
