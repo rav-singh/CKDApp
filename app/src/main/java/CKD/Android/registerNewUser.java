2ae976bb0ff9633f4ff392f5b61a485eb81e23ad
MattKazen <Matthew.Kazen@gmail.com>
2018-03-01 13:30:44 -0500

    Add Checks To make sure User enters data

--- app/src/main/java/CKD/Android/registerNewUser.java
+++ app/src/main/java/CKD/Android/registerNewUser.java
@@ -15,8 +15,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 
-public class registerNewUser extends AppCompatActivity {
-
+public class registerNewUser extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -28,10 +28,6 @@ public class registerNewUser extends AppCompatActivity {
         final EditText Password = findViewById(R.id.Register_TF_Password);
         final Button Register = findViewById(R.id.Register_Btn_Register);
 
-        //TODO Used for debugging
-/*        Email.setText("Matt@gmail.com");
-        Password.setText("123123"); */
-
         // OnClick Listener that redirects to Register Page
         Register.setOnClickListener(new View.OnClickListener()
         {
@@ -40,30 +36,46 @@ public class registerNewUser extends AppCompatActivity {
                 final String newEmail = Email.getText().toString();
                 final String newPassword = Password.getText().toString();
 
-                //TODO create second Password for verification and check ==
-                //TODO create second if statement for appropriate Toast
-                if(fieldsAreEmpty(newEmail, newPassword) && validEmail(newEmail))
+                if(eitherFieldIsEmpty(newEmail, newPassword))
                 {
                     Toast.makeText(registerNewUser.this,
                                     "Please Fill in Both Text Fields",
                                     Toast.LENGTH_LONG).show();
+                    return;
                 }
-
-                else
+                // Makes sure users entered email address is valid
+                // Contains "@" and ".com"
+                if(!validEmail(newEmail))
                 {
-                    // Creates UserClass instance in AppData with only the Email instantiated
-                   // AppData.cur_user = new UserClass(null, newEmail, null,
-                     //       null, null, null);
-                    AppData.setCur_User((new UserClass( null, newEmail, null,null,
-                            null,null)));
-                    // holds the users password
-                    AppData.cur_user.setPassword(newPassword);
-
-                    Intent launchActivity1= new Intent(CKD.Android.registerNewUser.this,Demographics_1.class);
-                    startActivity(launchActivity1);
+                    Toast.makeText(registerNewUser.this,
+                            "Please use a valid email Address!",
+                            Toast.LENGTH_LONG).show();
+                    return;
                 }
-             }
+                // Password must be atleast 6 characters long
+                if(!validPassword(newPassword))
+                {
+                    Toast.makeText(registerNewUser.this,
+                            "Password must be atleast 6 characters long!",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                // Creates UserClass instance in AppData with only the Email instantiated
+                AppData.setCur_User((new UserClass( null, newEmail, null,null,
+                        null,null)));
+                // holds the users password
+                AppData.cur_user.setPassword(newPassword);
+
+                Intent launchActivity1= new Intent(CKD.Android.registerNewUser.this,Demographics_1.class);
+                startActivity(launchActivity1);
+            }
         });
+    }
+
+    private boolean validPassword(String newPassword)
+    {
+        return newPassword.length() >= 6;
     }
 
     private boolean validEmail(String newEmail)
@@ -72,7 +84,7 @@ public class registerNewUser extends AppCompatActivity {
                newEmail.contains(".com");
     }
 
-    private boolean fieldsAreEmpty(String newEmail, String newPassword)
+    private boolean eitherFieldIsEmpty(String newEmail, String newPassword)
     {
         return newEmail.isEmpty() || newPassword.isEmpty();
     }
