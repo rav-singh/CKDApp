d5daa09ad413f793b249842291449d31fc60a999
MattKazen <Matthew.Kazen@gmail.com>
2018-03-11 17:48:26 -0400

    User's UID is placed into the Database under the Admins they have selected. The Date is also placed into the database for future implementations of temporary access

--- app/src/main/java/CKD/Android/Summary.java
+++ app/src/main/java/CKD/Android/Summary.java
@@ -3,7 +3,9 @@ package CKD.Android;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,17 +22,26 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 
 public class Summary extends AppCompatActivity
 {
     private Button  admin1, admin2,
-            admin3, admin4,
-            admin5, export;
+                    admin3, admin4,
+                    admin5, export;
+
     private DataSnapshot DS;
-    private List<String> adminsList = new ArrayList<>();
+
+    private List<String> adminsUIDList = new ArrayList<>();
+    private List<String> adminsnameList = new ArrayList<>();
+    private Map<String,String> selectedAdmins= new HashMap<>();
     private List<Button> adminBtnList = new ArrayList<>();
 
     @Override
@@ -42,6 +54,68 @@ public class Summary extends AppCompatActivity
 
         fillInAdminButtons();
 
+
+        int i =0;
+        for( Button b : adminBtnList)
+        {
+            setOnClickListeners(b, i++);
+        }
+
+        export.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                addPatientsUIDToSelectedAdmins();
+            }
+        });
+
+    }
+
+    private void addPatientsUIDToSelectedAdmins()
+    {
+        //TODO Need to make sure that the date still works appropriately when past the 10th
+        String date = new SimpleDateFormat("YYYY-MM-dd", Locale.getDefault()).format(new Date());
+        String userUID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        //Grabs references to the Root node and Users Node
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+
+        // Creates a Mood Child Node under Data
+        // Creates a UID Child Node under Mood
+        DatabaseReference Admin_node = db.getReference("Admins");
+
+        for(String adminUID : selectedAdmins.keySet())
+        {
+            DatabaseReference Patients_node = Admin_node.child(adminUID).child("Patients");
+            Patients_node.child(date).setValue(userUID);
+        }
+
+
+    }
+
+    private void setOnClickListeners(final Button b, final int i)
+    {
+        b.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                b.setSelected(!b.isSelected());
+
+                String adminUID = adminsUIDList.get(i);
+                String adminName = adminsnameList.get(i);
+
+                if(b.isSelected())
+                {
+                    selectedAdmins.put(adminUID, adminName);
+                    b.setBackgroundColor(Color.GREEN);
+                }
+                else
+                {
+                    selectedAdmins.remove(adminUID);
+                    b.setBackgroundColor(Color.WHITE);
+                }
+            }
+        });
     }
 
     private void fillInAdminButtons()
@@ -62,15 +136,15 @@ public class Summary extends AppCompatActivity
                 fillAdminUIDList();
 
                 int i = 0;
-                for(String adminKey : adminsList)
+                for(String adminKey : adminsUIDList)
                 {
                     fillInAdminButtons(adminKey, i++);
                 }
-
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onCancelled(DatabaseError databaseError)
+            {
 
             }
         });
@@ -78,14 +152,16 @@ public class Summary extends AppCompatActivity
 
     private void fillInAdminButtons(String UID, int i )
     {
-        adminBtnList.get(i).setText((String)DS.child(UID).child("Name").getValue());
+        String adminName = (String)DS.child(UID).child("Name").getValue();
+        adminBtnList.get(i).setText(adminName);
+        adminsnameList.add(adminName);
     }
 
     private void fillAdminUIDList()
     {
         for(DataSnapshot d : DS.getChildren())
         {
-            adminsList.add(d.getKey());
+            adminsUIDList.add(d.getKey());
         }
     }
 
@@ -102,5 +178,7 @@ public class Summary extends AppCompatActivity
         admin5 = findViewById(R.id.Summary_Admin5);
         adminBtnList.add(admin5);
         export = findViewById(R.id.Summary_Export);
+
+
     }
 }
