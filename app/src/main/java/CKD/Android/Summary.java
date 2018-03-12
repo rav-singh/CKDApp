702fd61ea2583a699640c03f3ad9f92e072558b8
MattKazen <Matthew.Kazen@gmail.com>
2018-03-12 14:09:55 -0400

    Added up to 10 possible buttons. Buttons List is now Scrollable. Buttons not filled are removed from the Linear Layout

--- app/src/main/java/CKD/Android/Summary.java
+++ app/src/main/java/CKD/Android/Summary.java
@@ -1,49 +1,38 @@
 package CKD.Android;
 
-import android.app.Activity;
+
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import org.w3c.dom.Text;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
 
 public class Summary extends AppCompatActivity
 {
-    private Button  admin1, admin2,
-                    admin3, admin4,
-                    admin5, export;
 
+    private Button export;
     private DataSnapshot DS;
 
     private List<String> adminsUIDList = new ArrayList<>();
     private List<String> adminsnameList = new ArrayList<>();
     private Map<String,String> selectedAdmins= new HashMap<>();
-    private List<Button> adminBtnList = new ArrayList<>();
-
+    private List<Button> allAdminBtnList = new ArrayList<>();
+    private List<Button> activeAdminBtnList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -55,8 +44,8 @@ public class Summary extends AppCompatActivity
         fillInAdminButtons();
 
 
-        int i =0;
-        for( Button b : adminBtnList)
+        int i = 0;
+        for( Button b : activeAdminBtnList)
         {
             setOnClickListeners(b, i++);
         }
@@ -70,29 +59,35 @@ public class Summary extends AppCompatActivity
                 startActivity(launchActivity1);
             }
         });
+    }
 
+    private void UpdateUI()
+    {
+        LinearLayout ll = findViewById(R.id.Summary_AdminListView);
+
+        // If a button has in the list of all buttons has not been activated
+        // then remove it from the Linear Layout
+        for(Button b : allAdminBtnList)
+        {
+            if(!activeAdminBtnList.contains(b))
+                ll.removeView(b);
+        }
     }
 
     private void addPatientsUIDToSelectedAdmins()
     {
-        //TODO Need to make sure that the date still works appropriately when past the 10th
-        String date = new SimpleDateFormat("YYYY-MM-dd", Locale.getDefault()).format(new Date());
+        // String date = new SimpleDateFormat("YYYY-MM-dd", Locale.getDefault()).format(new Date());
         String userUID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-        //Grabs references to the Root node and Users Node
         FirebaseDatabase db = FirebaseDatabase.getInstance();
 
-        // Creates a Mood Child Node under Data
-        // Creates a UID Child Node under Mood
         DatabaseReference Admin_node = db.getReference("Admins");
 
         for(String adminUID : selectedAdmins.keySet())
         {
             DatabaseReference Patients_node = Admin_node.child(adminUID).child("Patients");
-            Patients_node.child(date).setValue(userUID);
+            Patients_node.child(AppData.cur_user.getName()).setValue(userUID);
         }
-
-
     }
 
     private void setOnClickListeners(final Button b, final int i)
@@ -133,8 +128,7 @@ public class Summary extends AppCompatActivity
             {
                 DS = dataSnapshot;
 
-                // Grabs all of the admins UID's and places them into
-                // and List
+                // Grabs all of the admins UID's and places them into a List
                 fillAdminUIDList();
 
                 int i = 0;
@@ -142,6 +136,7 @@ public class Summary extends AppCompatActivity
                 {
                     fillInAdminButtons(adminKey, i++);
                 }
+                UpdateUI();
             }
 
             @Override
@@ -151,12 +146,18 @@ public class Summary extends AppCompatActivity
             }
         });
     }
-
+    // Grabs Admins Name from Database and sets the text on that button
+    // to said name. Then adds the admins name to the list in the same index
+    // as well as adding that button to the activated Buttons list
     private void fillInAdminButtons(String UID, int i )
     {
         String adminName = (String)DS.child(UID).child("Name").getValue();
-        adminBtnList.get(i).setText(adminName);
+
+        allAdminBtnList.get(i).setText(adminName);
+
         adminsnameList.add(adminName);
+
+        activeAdminBtnList.add(allAdminBtnList.get(i));
     }
 
     private void fillAdminUIDList()
@@ -169,16 +170,27 @@ public class Summary extends AppCompatActivity
 
     private void initializeButtons()
     {
-        admin1 = findViewById(R.id.Summary_Admin1);
-        adminBtnList.add(admin1);
-        admin2 = findViewById(R.id.Summary_Admin2);
-        adminBtnList.add(admin2);
-        admin3 = findViewById(R.id.Summary_Admin3);
-        adminBtnList.add(admin3);
-        admin4 = findViewById(R.id.Summary_Admin4);
-        adminBtnList.add(admin4);
-        admin5 = findViewById(R.id.Summary_Admin5);
-        adminBtnList.add(admin5);
+        Button admin1 = findViewById(R.id.Summary_Admin1);
+        allAdminBtnList.add(admin1);
+        Button admin2 = findViewById(R.id.Summary_Admin2);
+        allAdminBtnList.add(admin2);
+        Button admin3 = findViewById(R.id.Summary_Admin3);
+        allAdminBtnList.add(admin3);
+        Button admin4 = findViewById(R.id.Summary_Admin4);
+        allAdminBtnList.add(admin4);
+        Button admin5 = findViewById(R.id.Summary_Admin5);
+        allAdminBtnList.add(admin5);
+        Button admin6 = findViewById(R.id.Summary_Admin6);
+        allAdminBtnList.add(admin6);
+        Button admin7 = findViewById(R.id.Summary_Admin7);
+        allAdminBtnList.add(admin7);
+        Button admin8 = findViewById(R.id.Summary_Admin8);
+        allAdminBtnList.add(admin8);
+        Button admin9 = findViewById(R.id.Summary_Admin9);
+        allAdminBtnList.add(admin9);
+        Button admin10 = findViewById(R.id.Summary_Admin10);
+        allAdminBtnList.add(admin10);
+
         export = findViewById(R.id.Summary_Export);
 
 
