c4c0254b15294480c9e8d8abd0534b1353ac6036
MattKazen <Matthew.Kazen@gmail.com>
2018-03-24 21:50:22 -0400

    AppData now has getTodaysDate and getYesterdaysDate methods to call at any time. UpdateDailyChecklist method added to call at any time.

--- app/src/main/java/CKD/Android/AppData.java
+++ app/src/main/java/CKD/Android/AppData.java
@@ -2,9 +2,16 @@ package CKD.Android;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.security.PublicKey;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 
 public final class  AppData
@@ -61,4 +68,48 @@ public final class  AppData
 
     static void setCurrentThread(ThreadClass cur_Thread){AppData.cur_Thread = cur_Thread;}
 
+    static void updateDailyChecklist(String component)
+    {
+        String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        String date = getTodaysDate();
+        //Grabs references to the Root node and Users Node
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+
+        DatabaseReference Data_node = db.getReference("Data");
+        DatabaseReference Mood_node = Data_node.child("DailyCheckList");
+        DatabaseReference UID_node = Mood_node.child(UID);
+
+        UID_node.child(date).child(component).setValue("1");
+    }
+
+    static String getTodaysDate()
+    {
+        DateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+
+        // Create a calendar object with today date.
+        Calendar calendar = Calendar.getInstance();
+
+        // Get current date of calendar which point to the yesterday now
+        Date today = calendar.getTime();
+
+        return dateFormat.format(today);
+    }
+
+    static String getYesterdaysDate()
+    {
+        DateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+
+        // Create a calendar object with today date.
+        Calendar calendar = Calendar.getInstance();
+
+        // Move calendar to yesterday
+        calendar.add(Calendar.DATE, -1);
+
+        // Get current date of calendar which point to the yesterday now
+        Date yesterday = calendar.getTime();
+
+        return dateFormat.format(yesterday);
+    }
 }
