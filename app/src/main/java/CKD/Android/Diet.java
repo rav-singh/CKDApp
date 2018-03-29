4da0ea44bc05153ab5221b05d64287e57c4dc446
MattKazen <Matthew.Kazen@gmail.com>
2018-03-29 18:42:39 -0400

    User is now brought back to diet page with most recent submission into database displayed on page.
    Once user has selected an item, they are prompted with a quantity question
    which is added to the database in a list, and presented to them instead of
    the ndbno number on diet.class page

--- app/src/main/java/CKD/Android/Diet.java
+++ app/src/main/java/CKD/Android/Diet.java
@@ -55,20 +55,21 @@ public class Diet extends AppCompatActivity {
     private Button btnAddSnacks;
     private String thisFoodName;
     private int thisFoodNbdNo;
+    private String thisFoodQuantity;
 
     // Arraylists for foodNames and Ndbnos for each meal
-    ArrayList<String> breakfastNames, breakfastNdbnos,
-                        lunchNames, lunchNdbnos,
-                        dinnerNames, dinnerNdbnos,
-                        snackNames, snackNdbnos;
+    ArrayList<String> breakfastNames, breakfastNdbnos, breakfastQuantity,
+                        lunchNames, lunchNdbnos,lunchQuantity,
+                        dinnerNames, dinnerNdbnos, dinnerQuantity,
+                        snackNames, snackNdbnos, snackQuantity;
 
     Map <String,ArrayList<String>> mealNameLists = new HashMap<>();
     Map <String,ArrayList<String>> mealNdbnoLists = new HashMap<>();
+    Map <String,ArrayList<String>> mealQuantityLists = new HashMap<>();
     Map <String, LinearLayout> llList = new HashMap<>();
 
     String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
     FirebaseDatabase db = FirebaseDatabase.getInstance();
-    private DatabaseReference mDatabse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -112,24 +113,35 @@ public class Diet extends AppCompatActivity {
         breakfastNdbnos = new ArrayList<>();
         mealNdbnoLists.put("Breakfast", breakfastNdbnos);
 
+        breakfastQuantity = new ArrayList<>();
+        mealQuantityLists.put("Breakfast", breakfastQuantity);
+
         lunchNames = new ArrayList<>();
         mealNameLists.put("Lunch",lunchNames);
 
         lunchNdbnos = new ArrayList<>();
         mealNdbnoLists.put("Lunch",lunchNdbnos);
 
+        lunchQuantity = new ArrayList<>();
+        mealQuantityLists.put("Lunch",lunchQuantity);
+
         dinnerNames = new ArrayList<>();
         mealNameLists.put("Dinner",dinnerNames);
 
         dinnerNdbnos = new ArrayList<>();
-
         mealNdbnoLists.put("Dinner", dinnerNdbnos);
+
+        dinnerQuantity = new ArrayList<>();
+        mealQuantityLists.put("Dinner", dinnerQuantity);
 
         snackNames = new ArrayList<>();
         mealNameLists.put("Snacks",snackNames);
 
         snackNdbnos = new ArrayList<>();
         mealNdbnoLists.put("Snacks",snackNdbnos);
+
+        snackQuantity = new ArrayList<>();
+        mealQuantityLists.put("Snacks",snackQuantity);
 
     }
 
@@ -215,7 +227,8 @@ public class Diet extends AppCompatActivity {
             private void grabMealSubmitted(String meal, DataSnapshot d)
             {
                 String foodID = (String) d.child("Food NDBs").getValue();
-                String foodNames =(String) d.child("Food Names").getValue();
+                String foodNames = (String) d.child("Food Names").getValue();
+                String foodQuantity = (String) d.child("Food Quantity").getValue();
 
                 assert foodNames != null;
                 foodNames = foodNames.replace("[" ,"");
@@ -225,15 +238,22 @@ public class Diet extends AppCompatActivity {
                 foodID = foodID.replace("[","");
                 foodID = foodID.replace("]","");
 
+                assert foodQuantity != null;
+                foodQuantity = foodQuantity.replace("[","");
+                foodQuantity = foodQuantity.replace("]","");
+
                 // Commas within the Name of each food item are currently replaced with "_"
                 ArrayList<String> ndbnoList = new ArrayList<>(Arrays.asList(foodID.split(",")));
                 ArrayList<String> nameList  = new ArrayList<>(Arrays.asList(foodNames.split(",")));
+                ArrayList<String> quantityList  = new ArrayList<>(Arrays.asList(foodQuantity.split(",")));
+
 
                 // Stores the data into the HashMap for corresponding meal
                 mealNameLists.get(meal).addAll(nameList);
                 mealNdbnoLists.get(meal).addAll(ndbnoList);
+                mealQuantityLists.get(meal).addAll(quantityList);
 
-                addFoodItems(meal);
+                updateUI(meal);
             }
 
             @Override
@@ -245,11 +265,11 @@ public class Diet extends AppCompatActivity {
 
     }
 
-    private void addFoodItems( String meal)
+    private void updateUI (String meal)
      {
         LinearLayout linear = llList.get(meal);
         ArrayList<String> nameList = mealNameLists.get(meal);
-        ArrayList<String> ndbnoList = mealNdbnoLists.get(meal);
+        ArrayList<String> quantityList = mealQuantityLists.get(meal);
 
         for (int j = 0; j < nameList.size() ; j++)
         {
@@ -264,13 +284,13 @@ public class Diet extends AppCompatActivity {
             childLayout.setLayoutParams(linearParams);
 
             TextView fName = new TextView(this);
-            TextView fID = new TextView(this);
+            TextView fquantity = new TextView(this);
 
             fName.setLayoutParams(new TableLayout.LayoutParams(
                     TableLayout.LayoutParams.WRAP_CONTENT,
                     TableLayout.LayoutParams.WRAP_CONTENT, 1f));
 
-            fID.setLayoutParams(new TableLayout.LayoutParams(
+            fquantity.setLayoutParams(new TableLayout.LayoutParams(
                     TableLayout.LayoutParams.WRAP_CONTENT,
                     TableLayout.LayoutParams.WRAP_CONTENT, 1f));
 
@@ -279,16 +299,16 @@ public class Diet extends AppCompatActivity {
             fName.setTypeface(Typeface.DEFAULT_BOLD);
             fName.setGravity(Gravity.START | Gravity.CENTER);
 
-            fID.setTextSize(16);
-            fID.setPadding(5, 3, 0, 3);
-            fID.setTypeface(null, Typeface.ITALIC);
-            fID.setGravity(Gravity.START | Gravity.CENTER);
+            fquantity.setTextSize(16);
+            fquantity.setPadding(5, 3, 0, 3);
+            fquantity.setTypeface(null, Typeface.ITALIC);
+            fquantity.setGravity(Gravity.START | Gravity.CENTER);
 
             fName.setText(nameList.get(j).replaceAll("_",","));
-            fID.setText(ndbnoList.get(j));
+            fquantity.setText("Quantity: ".concat(quantityList.get(j)));
 
             childLayout.addView(fName);
-            childLayout.addView(fID);
+            childLayout.addView(fquantity);
 
             linear.addView(childLayout);
         }
@@ -296,21 +316,16 @@ public class Diet extends AppCompatActivity {
 
     // Listener for result back from SearchFood
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String intrequestcode = Integer.toString(requestCode);
-        Log.i("request code was: ", intrequestcode);
-
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
         if(resultCode == Activity.RESULT_OK)
         {
             thisFoodName = data.getStringExtra("FoodName");
             thisFoodNbdNo = data.getIntExtra("NdbNo", 0);
-            String intFoodNdbNo = Integer.toString(thisFoodNbdNo);
+            thisFoodQuantity = data.getStringExtra("Quantity");
 
-            Log.i("Logging foodName: " ,thisFoodName);
-            Log.i("Logging NdbNo: " ,intFoodNdbNo);
-
-            enterSelectedFoodItemIntoDatabase(requestCode, thisFoodName, thisFoodNbdNo );
-
+            enterSelectedFoodItemIntoDatabase(requestCode, thisFoodName, thisFoodNbdNo,thisFoodQuantity);
+            updateUI(getMeal(requestCode));
         }
 
         else
@@ -319,24 +334,28 @@ public class Diet extends AppCompatActivity {
         }
     }
 
-    private void enterSelectedFoodItemIntoDatabase(int requestCode, String thisFoodName, int thisFoodNbdNo)
+    private void enterSelectedFoodItemIntoDatabase(int requestCode, String thisFoodName, int thisFoodNbdNo, String quantity)
     {
         String meal = getMeal(requestCode);
         String date = AppData.getTodaysDate();
 
+        // Navigates to the correct date under the corresponding user
         DatabaseReference Diet_node = db.getReference().child("Data").child("Diet");
         DatabaseReference Date_node = Diet_node
-                .child(UID)
-                .child(date);
+                                        .child(UID)
+                                        .child(date);
 
         mealNameLists.get(meal).add(thisFoodName);
         mealNdbnoLists.get(meal).add(String.valueOf(thisFoodNbdNo));
+        mealQuantityLists.get(meal).add(quantity);
 
-        String bnames = mealNameLists.get(meal).toString();
-        String bndbs = mealNdbnoLists.get(meal).toString();
+        String fnames = mealNameLists.get(meal).toString();
+        String fndbs = mealNdbnoLists.get(meal).toString();
+        String fQuantity = mealQuantityLists.get(meal).toString();
 
-        Date_node.child(meal).child("Food Names").setValue(bnames);
-        Date_node.child(meal).child("Food NDBs").setValue(bndbs);
+        Date_node.child(meal).child("Food Names").setValue(fnames);
+        Date_node.child(meal).child("Food NDBs").setValue(fndbs);
+        Date_node.child(meal).child("Food Quantity").setValue(fQuantity);
     }
 
     private String getMeal(int requestCode)
