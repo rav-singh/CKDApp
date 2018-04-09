78c134ff7620f004a282f5322fbbafa2111788cf
MattKazen <Matthew.Kazen@gmail.com>
2018-04-09 01:18:44 -0400

    Commented out lots of unnecessary code as once a user is registered to the database we no longer need to store all of the additional values in the user class.

--- app/src/main/java/CKD/Android/LoginActivity.java
+++ app/src/main/java/CKD/Android/LoginActivity.java
@@ -96,75 +96,27 @@ public class LoginActivity extends AppCompatActivity
 
         DatabaseReference users_node = AppData.db.getReference("Users");
         DatabaseReference cur_user_node = users_node.child(userUID);
-        DatabaseReference add_node = users_node.child(userUID).child("Additional");
+//        DatabaseReference add_node = users_node.child(userUID).child("Additional");
 
-        readFromDatabaseAndWriteToAppData(cur_user_node,"Name");
-        readFromDatabaseAndWriteToAppData(cur_user_node,"Email");
-        readFromDatabaseAndWriteToAppData(cur_user_node,"Phone Number");
-        readFromDatabaseAndWriteToAppData(add_node,"Activity Level");
-        readFromDatabaseAndWriteToAppData(add_node,"Age");
-        readFromDatabaseAndWriteToAppData(add_node,"Education");
-        readFromDatabaseAndWriteToAppData(add_node,"Gender");
-        readFromDatabaseAndWriteToAppData(add_node,"Health");
-        readFromDatabaseAndWriteToAppData(add_node,"Marital Status");
-        readFromDatabaseAndWriteToAppData(add_node,"Work");
+        AppData.readFromDatabaseAndWriteToAppData(cur_user_node,"Name");
+        AppData.readFromDatabaseAndWriteToAppData(cur_user_node,"Email");
+        AppData.readFromDatabaseAndWriteToAppData(cur_user_node,"Phone Number");
+
+        //Commented out as we do not need to store this data on the phone Is only use
+        // Only used to store in the database
+
+        /*   AppData.readFromDatabaseAndWriteToAppData(add_node,"Activity Level");
+        AppData.readFromDatabaseAndWriteToAppData(add_node,"Age");
+        AppData.readFromDatabaseAndWriteToAppData(add_node,"Education");
+        AppData.readFromDatabaseAndWriteToAppData(add_node,"Gender");
+        AppData.readFromDatabaseAndWriteToAppData(add_node,"Health");
+        AppData.readFromDatabaseAndWriteToAppData(add_node,"Marital Status");
+        AppData.readFromDatabaseAndWriteToAppData(add_node,"Work");  */
+
+
     }
 
-    private void readFromDatabaseAndWriteToAppData(DatabaseReference node, final String key)
-    {
-        node.child(key).addListenerForSingleValueEvent(new ValueEventListener()
-        {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot)
-            {
 
-                // Returns the value under the node passed in at the key passed in
-                String value = dataSnapshot.getValue(String.class);
-
-                if(Objects.equals(key, "Name"))
-                    AppData.cur_user.setName(value);
-
-                else if(Objects.equals(key, "Email"))
-                   AppData.cur_user.setEmail(value);
-
-                else if (Objects.equals(key, "Phone Number"))
-                    AppData.cur_user.setPhone(value);
-
-                else if (Objects.equals(key, "Age"))
-                    AppData.cur_user.setAge(value);
-
-                else if (Objects.equals(key, "Activity Level"))
-                    AppData.cur_user.setActivityLevel(value);
-
-                else if (Objects.equals(key, "Marital Status"))
-                    AppData.cur_user.setMarital(value);
-
-                else if (Objects.equals(key, "Gender"))
-                    AppData.cur_user.setGender(value);
-
-                else if (Objects.equals(key, "Education"))
-                    AppData.cur_user.setEducation(value);
-
-                else if (Objects.equals(key, "Work"))
-                    AppData.cur_user.setWork(value);
-
-                else if (Objects.equals(key, "Health"))
-                    AppData.cur_user.setHealth(value);
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError)
-            {
-                //TODO What happens when theres an Error
-                Log.i("DATABASE READ ERROR",
-                        databaseError.getMessage().toString());
-                Log.i("DATABASE READ ERROR",
-                        databaseError.getMessage().toString());
-                Log.i("DATABASE READ ERROR",
-                        databaseError.getMessage().toString());
-            }});
-    }
 
     private void updateFirebaseInAppData()
     {
