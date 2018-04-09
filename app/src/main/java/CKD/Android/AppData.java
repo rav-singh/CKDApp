3ec1ff452e8df3d77e456bb68ff8c4ad80d3f9ed
MattKazen <Matthew.Kazen@gmail.com>
2018-04-09 01:15:04 -0400

    Moved repeatitive methods from MainActivity and Login into Appdata Class

--- app/src/main/java/CKD/Android/AppData.java
+++ app/src/main/java/CKD/Android/AppData.java
@@ -2,6 +2,7 @@ package CKD.Android;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +18,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Objects;
 import java.util.TimeZone;
 
 
@@ -166,6 +168,7 @@ public final class  AppData
             @Override
             public void onDataChange(DataSnapshot dataSnapshot)
             {
+
                 Integer count = dataSnapshot.getValue(Integer.class);
 
                 if(count == null)
@@ -199,5 +202,122 @@ public final class  AppData
             }
         });
         return b;
+    }
+
+    static void readFromDatabaseAndWriteToAppData(DatabaseReference node, final String key)
+    {
+        node.child(key).addListenerForSingleValueEvent(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                // Returns the value under the node passed in at the key passed in
+                String value = dataSnapshot.getValue(String.class);
+
+                if(Objects.equals(key, "Name"))
+                    AppData.cur_user.setName(value);
+
+                else if(Objects.equals(key, "Email"))
+                    AppData.cur_user.setEmail(value);
+
+                else if (Objects.equals(key, "Phone Number"))
+                    AppData.cur_user.setPhone(value);
+/*
+                else if (Objects.equals(key, "Age"))
+                    AppData.cur_user.setAge(value);
+
+                else if (Objects.equals(key, "Activity Level"))
+                    AppData.cur_user.setActivityLevel(value);
+
+                else if (Objects.equals(key, "Marital Status"))
+                    AppData.cur_user.setMarital(value);
+
+                else if (Objects.equals(key, "Gender"))
+                    AppData.cur_user.setGender(value);
+
+                else if (Objects.equals(key, "Education"))
+                    AppData.cur_user.setEducation(value);
+
+                else if (Objects.equals(key, "Work"))
+                    AppData.cur_user.setWork(value);
+
+                else if (Objects.equals(key, "Health"))
+                    AppData.cur_user.setHealth(value);
+*/
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError)
+            {
+                //TODO What happens when theres an Error
+                Log.i("DATABASE READ ERROR",
+                        databaseError.getMessage().toString());
+                Log.i("DATABASE READ ERROR",
+                        databaseError.getMessage().toString());
+                Log.i("DATABASE READ ERROR",
+                        databaseError.getMessage().toString());
+            }});
+    }
+
+    private static void WriteTimesToAppData(DatabaseReference schedule_node)
+    {
+        for(final String Day : AppData.cur_user.getScheduledDays())
+        {
+            schedule_node.child(Day).child("StartTime").addListenerForSingleValueEvent(new ValueEventListener()
+            {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot)
+                {
+                    String startTime = dataSnapshot.getValue(String.class);
+                    AppData.cur_user.getScheduledStartTime().put(Day, startTime);
+
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+            schedule_node.child(Day).child("EndTime").addListenerForSingleValueEvent(new ValueEventListener()
+            {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot)
+                {
+                    String EndTime = dataSnapshot.getValue(String.class);
+                    AppData.cur_user.getScheduledStartTime().put(Day, EndTime);
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError)
+                {
+
+                }
+            });
+        }
+    }
+
+
+    static void WriteDaysToAppData(final DatabaseReference schedule_node)
+    {
+        schedule_node.addListenerForSingleValueEvent(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                for(DataSnapshot d : dataSnapshot.getChildren())
+                {
+                    // Adds all the days the user is scheduled for to the list inside
+                    // AppData.cur_user class
+                    String Day = d.getKey();
+                    AppData.cur_user.getScheduledDays().add(Day);
+                }
+
+                WriteTimesToAppData(schedule_node);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
