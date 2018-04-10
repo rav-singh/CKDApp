3769559ac86c5d8334b704ed759d2476ff7b13f9
Matheus Goncalves <matheus@knights.ucf.edu>
2018-04-10 16:20:14 -0400

    Created page which will contain the Video component.

---
+++ app/src/main/java/CKD/Android/Videos.java
@@ -0,0 +1,13 @@
+package CKD.Android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Videos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_videos);
+    }
+}
