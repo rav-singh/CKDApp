0c634786e930e82f2f07292071247a4b900a8adb
MattKazen <Matthew.Kazen@gmail.com>
2018-04-16 18:35:30 -0400

    Updated Formatting UI. Dynamically adds activities and switched between two colors. First activity name has top left corner rounded. First activity duration has top right corner rounded. Last activity name has bottom left corner rounded. Last duration has bottom right corner rounded. Correctly updates corners when new activity and duration is added

--- app/src/main/java/CKD/Android/ActivityLog.java
+++ app/src/main/java/CKD/Android/ActivityLog.java
@@ -2,7 +2,13 @@ package CKD.Android;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RoundRectShape;
+import android.graphics.drawable.shapes.Shape;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -39,6 +45,14 @@ public class ActivityLog extends AppCompatActivity {
     ArrayList<String> ActivityDurations = new ArrayList<>();
     LinearLayout activitiesLL;
 
+    final float[] topLeftCorner = new float[]{20.0f, 20.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+    final float[] topRightCorner = new float[]{0.0f, 0.0f, 20.0f, 20.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+    final float[] botRightCorner = new float[]{0.0f, 0.0f, 0.0f, 0.0f, 20.0f, 20.0f, 0.0f, 0.0f};
+    final float[] botLeftCorner = new float[]{0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 20.0f, 20.0f};
+    final float[] leftCorners = new float[]{20.0f, 20.0f, 0.0f, 0.0f, 0.0f, 0.0f, 20.0f, 20.0f};
+    final float[] rightCorners = new float[]{0.0f, 0.0f, 20.0f, 20.0f, 20.0f, 20.0f, 0.0f, 0.0f};
+    int colorA;
+    int colorB;
 
     FirebaseDatabase db = FirebaseDatabase.getInstance();
     String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
@@ -49,9 +63,13 @@ public class ActivityLog extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
 
+        colorA = getResources().getColor(R.color.LightGreen);
+        colorB = getResources().getColor(R.color.LightCoral);
+
         addActivity = findViewById(R.id.addActivity_Button);
         activityET = findViewById(R.id.activityInput);
         activitiesLL = findViewById(R.id.ActivityLog_LL);
+
         Button home = findViewById(R.id.activityLog_btn_home);
         home = AppData.activateHomeButton(home,ActivityLog.this);
 
@@ -62,7 +80,7 @@ public class ActivityLog extends AppCompatActivity {
         addActivity.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
             {
-                String newActivity = activityET.getText().toString();
+                String newActivity = activityET.getText().toString().trim();
 
                 // Makes sure user does not enter empty string or an exercise with a comma
                 //TODO TOAST ERROR
@@ -85,7 +103,7 @@ public class ActivityLog extends AppCompatActivity {
 
                 final EditText input = new EditText(ActivityLog.this);
 
-                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                input.setInputType(InputType.TYPE_CLASS_NUMBER);
                 ll.addView(input);
                 builder.setView(ll);
 
@@ -93,7 +111,7 @@ public class ActivityLog extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which)
                     {
-                        duration = input.getText().toString();
+                        duration = input.getText().toString().trim();
 
                         ActivityDurations.add(duration);
 
@@ -167,49 +185,156 @@ public class ActivityLog extends AppCompatActivity {
     {
         int start = activitiesLL.getChildCount();
 
-        RelativeLayout rl = findViewById(R.id.ActivityLog_RL);
-        int width = (int) (rl.getWidth()*.5);
-
         LinearLayout.LayoutParams params_LL = new LinearLayout.LayoutParams
                 (LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
-        LinearLayout.LayoutParams params_TV = new LinearLayout.LayoutParams
-                (width, LinearLayout.LayoutParams.WRAP_CONTENT);
-        params_TV.setMargins(10,0,10,0);
+        LinearLayout.LayoutParams params_activity = new LinearLayout.LayoutParams
+                (0, LinearLayout.LayoutParams.WRAP_CONTENT,4);
 
+
+        LinearLayout.LayoutParams params_duration = new LinearLayout.LayoutParams
+                (0, LinearLayout.LayoutParams.WRAP_CONTENT,1);
 
         // For each activity it creates two Text Views (One for name and one for time)
         // Both of those are added into a horizontal linear Layout. That linear layout
         // is then added into a vertical linear layout
         for(int i = start;  i < ActivityNames.size() ; i++)
         {
+            // If there are existing views in the layout, the
+            // rounded corners need to be removed for aesthetics
+            if(i!=0)
+            {
+                updateLastActivityCorners(i-1);
+            }
+
             LinearLayout singleActivity_ll = new LinearLayout(this);
+
             singleActivity_ll.setLayoutParams(params_LL);
             singleActivity_ll.setOrientation(LinearLayout.HORIZONTAL);
-
+            singleActivity_ll.setWeightSum(5);
 
             TextView name = new TextView(this);
             TextView duration = new TextView(this);
 
-            name.setLayoutParams(params_TV);
-            duration.setLayoutParams(params_TV);
+            name.setLayoutParams(params_activity);
+            duration.setLayoutParams(params_duration);
 
-            name.setTextSize(17);
-            name.setPadding(5, 3, 0, 3);
-            name.setTypeface(Typeface.DEFAULT_BOLD);
-            name.setText(ActivityNames.get(i));
+            name.setTextSize(20);
+            name.setTextColor(Color.BLACK);
+            name.setText(ActivityNames.get(i).trim());
+            name.setPadding(50,0,0,0);
 
-            duration.setTextSize(17);
-            duration.setPadding(5, 3, 0, 3);
-            duration.setTypeface(Typeface.DEFAULT_BOLD);
-            duration.setText(ActivityDurations.get(i));
+            duration.setTextSize(20);
+            duration.setTextColor(Color.BLACK);
+            duration.setText(ActivityDurations.get(i).trim());
+
+            GradientDrawable d1 = new GradientDrawable();
+            GradientDrawable d2 = new GradientDrawable();
+
+            // First and only Activity
+            if(i == 0 && i == ActivityNames.size()-1)
+            {
+                d1.setCornerRadii(leftCorners);
+                d1.setColor(colorA);
+                name.setBackground(d1);
+
+                d2.setCornerRadii(rightCorners);
+                d2.setColor(colorA);
+                duration.setBackground(d2);
+            }
+
+            // If first(but not last) activity add top corners
+            else if(i == 0)
+            {
+                d1.setCornerRadii(topLeftCorner);
+                d1.setColor(colorA);
+                name.setBackground(d1);
+
+                d2.setCornerRadii(topRightCorner);
+                d2.setColor(colorA);
+                duration.setBackground(d2);
+            }
+
+            // If last activity, make bottom corners
+            else if(i == ActivityNames.size()-1)
+            {
+                d1.setCornerRadii(botLeftCorner);
+                d2.setCornerRadii(botRightCorner);
+
+                // Decides which color the last one is
+                if(i%2 ==0)
+                {
+                    d1.setColor(colorA);
+                    d2.setColor(colorA);
+                }
+                else
+                {
+                    d1.setColor(colorB);
+                    d2.setColor(colorB);
+                }
+
+                name.setBackground(d1);
+                duration.setBackground(d2);
+
+            }
+            // Last two take care of middle section where no rounded
+            // corners are needed
+            else if(i%2 == 0 )
+            {
+                name.setBackgroundColor(colorA);
+                duration.setBackgroundColor(colorA);
+            }
+            else
+            {
+                name.setBackgroundColor(colorB);
+                duration.setBackgroundColor(colorB);
+            }
+
 
             singleActivity_ll.addView(name);
             singleActivity_ll.addView(duration);
-            singleActivity_ll.setBackground(getResources().getDrawable(R.drawable.rounded_corner_textview));
 
             activitiesLL.addView(singleActivity_ll);
         }
+
+
+    }
+
+    private void updateLastActivityCorners(int lastViewIndex)
+    {
+        GradientDrawable d1 = new GradientDrawable();
+        GradientDrawable d2 = new GradientDrawable();
+
+        LinearLayout lastActivity = (LinearLayout) activitiesLL.getChildAt(lastViewIndex);
+
+        TextView name = (TextView) lastActivity.getChildAt(0);
+        TextView duration = (TextView) lastActivity.getChildAt(1);
+
+        name.setBackground(null);
+        duration.setBackground(null);
+
+        if(lastViewIndex == 0)
+        {
+            d1.setCornerRadii(topLeftCorner);
+            d1.setColor(colorA);
+            d2.setCornerRadii(topRightCorner);
+            d2.setColor(colorA);
+
+            name.setBackground(d1);
+            duration.setBackground(d2);
+        }
+
+        else if(lastViewIndex %2 == 0)
+        {
+            name.setBackgroundColor(colorA);
+            duration.setBackgroundColor(colorA);
+        }
+        else
+        {
+            name.setBackgroundColor(colorB);
+            duration.setBackgroundColor(colorB);
+        }
+
 
 
     }
